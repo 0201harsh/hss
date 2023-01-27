3cfcdd1b00fa18ae79d17072c08db646cf947619
Harsh Vardhan Singh <103317695+0201harsh@users.noreply.github.com>
2023-01-27 12:11:52 +0530

    Add files via upload

---
+++ HarshSingh.Rcheck/00_pkg_src/HarshSingh/R/hvs.R
@@ -0,0 +1,11 @@
+#'Data Analysis
+#'@export
+#'@param v numeric
+basic_function = function(v){
+  data.frame(
+    min=min(v),
+    max=max(v),
+    range=max(v) - min(v),
+    std_dev=sd(v)
+  )
+}
